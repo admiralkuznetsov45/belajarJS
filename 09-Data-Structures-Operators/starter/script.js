92845df443b59b0565708ea74dddf75a3a430c2d
Muhammad Daffa ZW <andafsul4@gmail.com>
2022-02-02 19:44:53 +0700

    Belajar  Array Destructuring
    Belajar Array Destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,18 +12,55 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
+
+  order: function (starterIndex , mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//array original tidak terlalu terdampak , destructuring and unpacking aja
+const [x,y,z] = arr;
+console.log(x,y,z)
+//  
+let [main ,, secondary] = restaurant.categories;
+console.log(main,secondary);
+
+//swap nilai
+[main,secondary] = [secondary,main]
+console.log(main,secondary)
+
+//return dua nilai dalam satu fungsi
+const [starter, mainCourse] = restaurant.order(2,0);
+console.log(starter , mainCourse)
+
+//Nested Destructuring
+const nested = [2,4,[5,6]];
+
+const [i,,[j,k]] = nested
+console.log(i,j,k)
+
+//Default Value Undefined 
+
+const [p,q,r] = [8,2]
+//r nya undefined krn kosong
+//tapi kalau misal [p=1,q=1,r=1] maka output nya 8,2,1
+console.log(p,q,r)
